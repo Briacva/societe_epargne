10af2b07be6b2400fdeab296d2d0abfeb9159f3f
mlaisney <laisneymarvin@gmail.com>
2022-03-01 17:06:40 +0100

    create open bak account interface

--- src/main/Program.java
+++ src/main/Program.java
@@ -1,10 +1,13 @@
 package main;
 
+import interfaces.OuvrirCompteForm;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		OuvrirCompteForm newBankAccount = new OuvrirCompteForm();
+		newBankAccount.setVisible(true);
 	}
 
 }
