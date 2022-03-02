85f6976f336aaf3c2132e1c2c98ef3ad4fb069ba
Guillaumelsn <guillaumelesenne@gmail.com>
2022-03-02 09:29:02 +0100

    Interface Nouveau client

--- src/interfaces/CreationClientForm.java
+++ src/interfaces/CreationClientForm.java
@@ -1,9 +1,136 @@
 package interfaces;
 
 import javax.swing.JFrame;
+import javax.swing.*;
+import com.jgoodies.forms.factories.DefaultComponentFactory;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.CardLayout;
+import java.awt.Rectangle;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class CreationClientForm extends JFrame {
-	public CreationClientForm() {
-	}
 
+	public CreationClientForm() {
+		getContentPane().setForeground(new Color(0, 255, 0));
+		setTitle("Societe d'epargne");
+		setSize(1200,800);
+		getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Inscription nouveau client");
+		lblNewLabel.setForeground(Color.decode("#395B64"));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Ebrima", Font.BOLD, 32));
+		lblNewLabel.setBounds(0, 80, 1184, 35);
+		getContentPane().add(lblNewLabel);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setForeground(new Color(0, 0, 0));
+		textArea.setBounds(575, 160, 300, 30);
+		getContentPane().add(textArea);
+		
+		JLabel lblNewLabel_1 = new JLabel("Numero de compte :");
+		lblNewLabel_1.setForeground(Color.decode("#395B64"));
+		lblNewLabel_1.setFont(new Font("Georgia", Font.BOLD, 16));
+		lblNewLabel_1.setBounds(326, 160, 201, 30);
+		getContentPane().add(lblNewLabel_1);
+		
+		JTextArea textArea_1 = new JTextArea();
+		textArea_1.setBounds(575, 225, 300, 30);
+		getContentPane().add(textArea_1);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("Raison sociale :");
+		lblNewLabel_1_1.setForeground(Color.decode("#395B64"));
+		lblNewLabel_1_1.setFont(new Font("Georgia", Font.BOLD, 16));
+		lblNewLabel_1_1.setBounds(326, 225, 201, 30);
+		getContentPane().add(lblNewLabel_1_1);
+		
+		JTextArea textArea_2 = new JTextArea();
+		textArea_2.setBounds(575, 290, 300, 30);
+		getContentPane().add(textArea_2);
+		
+		JLabel lblNewLabel_1_2 = new JLabel("Nom :");
+		lblNewLabel_1_2.setForeground(Color.decode("#395B64"));
+		lblNewLabel_1_2.setFont(new Font("Georgia", Font.BOLD, 16));
+		lblNewLabel_1_2.setBounds(326, 290, 201, 30);
+		getContentPane().add(lblNewLabel_1_2);
+		
+		JTextArea textArea_3 = new JTextArea();
+		textArea_3.setBounds(575, 355, 300, 30);
+		getContentPane().add(textArea_3);
+		
+		JLabel lblNewLabel_1_3 = new JLabel("Prenom :");
+		lblNewLabel_1_3.setForeground(Color.decode("#395B64"));
+		lblNewLabel_1_3.setFont(new Font("Georgia", Font.BOLD, 16));
+		lblNewLabel_1_3.setBounds(326, 355, 201, 30);
+		getContentPane().add(lblNewLabel_1_3);
+		
+		JTextArea textArea_4 = new JTextArea();
+		textArea_4.setBounds(575, 420, 300, 30);
+		getContentPane().add(textArea_4);
+		
+		JLabel lblNewLabel_1_4 = new JLabel("Telephone :");
+		lblNewLabel_1_4.setForeground(Color.decode("#395B64"));
+		lblNewLabel_1_4.setFont(new Font("Georgia", Font.BOLD, 16));
+		lblNewLabel_1_4.setBounds(326, 420, 201, 30);
+		getContentPane().add(lblNewLabel_1_4);
+		
+		JTextArea textArea_5 = new JTextArea();
+		textArea_5.setBounds(575, 485, 300, 30);
+		getContentPane().add(textArea_5);
+		
+		JLabel lblNewLabel_1_5 = new JLabel("Civilite :");
+		lblNewLabel_1_5.setForeground(Color.decode("#395B64"));
+		lblNewLabel_1_5.setFont(new Font("Georgia", Font.BOLD, 16));
+		lblNewLabel_1_5.setBounds(326, 485, 201, 30);
+		getContentPane().add(lblNewLabel_1_5);
+		
+		JTextArea textArea_6 = new JTextArea();
+		textArea_6.setBounds(575, 550, 300, 60);
+		getContentPane().add(textArea_6);
+		
+		JLabel lblNewLabel_1_6 = new JLabel("Adresse :");
+		lblNewLabel_1_6.setForeground(Color.decode("#395B64"));
+		lblNewLabel_1_6.setFont(new Font("Georgia", Font.BOLD, 16));
+		lblNewLabel_1_6.setBounds(326, 550, 201, 30);
+		getContentPane().add(lblNewLabel_1_6);
+		
+		JButton btnNewButton = new JButton("Valider");
+		btnNewButton.setBackground(new Color(255, 255, 255));
+		btnNewButton.setFont(new Font("Ebrima", Font.PLAIN, 14));
+		btnNewButton.setBounds(300, 649, 150, 40);
+		getContentPane().add(btnNewButton);
+		
+		JButton btnAnnuler = new JButton("Annuler");
+		btnAnnuler.setBackground(new Color(255, 255, 255));
+		btnAnnuler.setFont(new Font("Ebrima", Font.PLAIN, 14));
+		btnAnnuler.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnAnnuler.setBounds(750, 649, 150, 40);
+		getContentPane().add(btnAnnuler);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(0.60f,0.60f,0.56f,0.85f));
+		panel.setBounds(260, 45, 680, 670);
+		getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JButton btnRinitialiser = new JButton("R\u00E9initialiser");
+		btnRinitialiser.setBackground(new Color(255, 255, 255));
+		btnRinitialiser.setFont(new Font("Ebrima", Font.PLAIN, 14));
+		btnRinitialiser.setBounds(270, 604, 150, 40);
+		panel.add(btnRinitialiser);
+		
+		JLabel lblNewLabel_2 = new JLabel("New label");
+		lblNewLabel_2.setIcon(new ImageIcon(CreationClientForm.class.getResource("/images/BGimage.jpg")));
+		lblNewLabel_2.setBounds(0, 0, 1184, 761);
+		getContentPane().add(lblNewLabel_2);
+		
+		
+		this.setVisible(true);
+	}
 }
