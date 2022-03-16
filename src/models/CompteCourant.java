decd1c729ead5998d6a357ea3da07ca5ab808396
marvinlaisney04 <laisneymarvin@gmail.com>
2022-03-16 11:20:12 +0100

    add comments

--- src/models/CompteCourant.java
+++ src/models/CompteCourant.java
@@ -5,30 +5,58 @@ package models;
 
 /**
  * @author marvin
- *
+ * Classe CompteCourant héritée de Compte
  */
 public class CompteCourant extends Compte{
+	// Variables de classe CompteCourant
 	private float soldeMinimum;
 	private float fraisTransfert;
 	
+	/**
+	 * Constructeur de CompteCourant, hérite des propriétés du parent pour l'instancier avec toutes les informations
+	 * @param id
+	 * @param numCompte
+	 * @param solde
+	 * @param soldeInitial
+	 * @param cloture
+	 * @param typeCompte
+	 * @param soldeMinimum
+	 * @param fraisTransfert
+	 */
 	public CompteCourant(int id, int numCompte, float solde, float soldeInitial, boolean cloture, boolean typeCompte, float soldeMinimum, float fraisTransfert) {
 		super(id, numCompte, solde, soldeInitial, cloture, typeCompte);
 		this.soldeMinimum = soldeMinimum;
 		this.fraisTransfert = fraisTransfert;
 	}
 
+	/**
+	 * Getter Solde minimum
+	 * @return
+	 */
 	public float getSoldeMinimum() {
 		return soldeMinimum;
 	}
 
+	/**
+	 * Setter Solde minimum
+	 * @param soldeMinimum
+	 */
 	public void setSoldeMinimum(float soldeMinimum) {
 		this.soldeMinimum = soldeMinimum;
 	}
 
+	/**
+	 * Getter Frais transfert
+	 * @return
+	 */
 	public float getFraisTransfert() {
 		return fraisTransfert;
 	}
 
+	/**
+	 * Setter Frais transfert
+	 * @param fraisTransfert
+	 */
 	public void setFraisTransfert(float fraisTransfert) {
 		this.fraisTransfert = fraisTransfert;
 	}
