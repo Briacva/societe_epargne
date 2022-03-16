e4ba34fab86724d0b17b4937c04897e69f0c14ad
Guillaumelsn <guillaumelesenne@gmail.com>
2022-03-16 09:52:31 +0100

    bobo

--- src/main/Program.java
+++ src/main/Program.java
@@ -1,9 +1,15 @@
 package main;
 
+import interfaces.CreationClientForm;
+
 public class Program {
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		CreationClientForm fenetre = new CreationClientForm();
+		
+		
 
 	}
 
