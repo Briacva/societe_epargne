3c652790a7727ac9d5ce0332fc3a8d5f75d852af
marvinlaisney04 <laisneymarvin@gmail.com>
2022-03-21 17:03:57 +0100

    remvoe unused import

--- src/main/Program.java
+++ src/main/Program.java
@@ -1,17 +1,12 @@
 package main;
 
-import java.sql.ResultSet;
-import java.sql.Statement;
-
 import interfaces.CreationClientForm;
-import interfaces.OuvrirCompteForm;
 
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		CreationClientForm NewClientForm = new CreationClientForm();
-		OuvrirCompteForm test = new OuvrirCompteForm();
 	}
 
 }
