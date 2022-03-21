08ffaa460c9271dbb4a7d56aa60585b9ec72573d
marvinlaisney04 <laisneymarvin@gmail.com>
2022-03-21 14:37:41 +0100

    delete lin in programs

--- src/main/Program.java
+++ src/main/Program.java
@@ -1,13 +1,9 @@
 package main;
 
-import interfaces.TransfererForm;
-
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		TransfererForm transfererForm = new TransfererForm();
-		transfererForm.setVisible(true);
 	}
 
 }
