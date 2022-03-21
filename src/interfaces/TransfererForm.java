9fbe245c75ee373bb7a0199f4681c6985b99ad27
marvinlaisney04 <laisneymarvin@gmail.com>
2022-03-21 12:35:18 +0100

    interface created

---
+++ src/interfaces/TransfererForm.java
@@ -0,0 +1,262 @@
+/**
+ * 
+ */
+package interfaces;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.SystemColor;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JSeparator;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
+
+import services.CompteService;
+import java.awt.Font;
+
+/**
+ * @author marvin
+ *
+ */
+public class TransfererForm extends JFrame {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JTextField textFieldMontant;
+	private JComboBox<String> comboBoxCompteSource;
+	private JComboBox<String> comboBoxCompteDestinataire;
+
+	private Hashtable<Integer, String> listCompteSource = new Hashtable<Integer, String>();
+	private Hashtable<Integer, String> listCompteDestinataire = new Hashtable<Integer, String>();
+	
+	public TransfererForm() {
+		// Instanciation du CompteService
+		CompteService compteService = new CompteService();
+		
+		setTitle("Transférer des fonds");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setResizable(false);
+		setPreferredSize(new Dimension(1200, 700));
+		getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 0, 1200, 700);
+		getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JPanel subPanelForm = new JPanel();
+		subPanelForm.setBounds(251, 49, 698, 597);
+		panel.add(subPanelForm);
+		subPanelForm.setBackground(new Color(0.60f,0.60f,0.56f,0.85f));
+		subPanelForm.setLayout(null);
+		
+		JLabel lblBackgroundAccountManagement = new JLabel("");
+		lblBackgroundAccountManagement.setVerticalAlignment(SwingConstants.BOTTOM);
+		lblBackgroundAccountManagement.setIcon(new ImageIcon(OuvrirCompteForm.class.getResource("/images/backgroundAccountManagement.jpg")));
+		lblBackgroundAccountManagement.setBounds(0, 0, 1200, 700);
+		panel.add(lblBackgroundAccountManagement);
+		
+		JLabel lblTitre = new JLabel("TRANSFERER DES FONDS");
+		lblTitre.setFont(new Font("Dialog", Font.BOLD, 22));
+		lblTitre.setBounds(168, 60, 328, 24);
+		subPanelForm.add(lblTitre);
+		
+		JSeparator separatorTitre = new JSeparator();
+		separatorTitre.setBackground(SystemColor.desktop);
+		separatorTitre.setForeground(new Color(0, 102, 102));
+		separatorTitre.setBounds(214, 90, 222, 15);
+		subPanelForm.add(separatorTitre);
+		
+		JLabel lblMontant = new JLabel("Montant :");
+		lblMontant.setFont(new Font("Dialog", Font.BOLD, 16));
+		lblMontant.setBackground(SystemColor.desktop);
+		lblMontant.setBounds(284, 402, 96, 24);
+		subPanelForm.add(lblMontant);
+		
+		JSeparator separatorMontant = new JSeparator();
+		separatorMontant.setBackground(SystemColor.desktop);
+		separatorMontant.setForeground(new Color(0, 102, 102));
+		separatorMontant.setBounds(275, 428, 96, 15);
+		subPanelForm.add(separatorMontant);
+		
+		textFieldMontant = new JTextField();
+		textFieldMontant.setForeground(Color.WHITE);
+		textFieldMontant.setColumns(10);
+		textFieldMontant.setBackground(new Color(51, 102, 102));
+		textFieldMontant.setBounds(150, 457, 370, 33);
+		textFieldMontant.setBorder(null);
+		subPanelForm.add(textFieldMontant);
+		
+		ButtonGroup radioButtonGroup = new ButtonGroup();
+		
+		JButton btnCreer = new JButton("Créer");
+		btnCreer.setForeground(new Color(255, 255, 255));
+		btnCreer.setBackground(new Color(30, 125, 125));
+		btnCreer.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//Récupération de l'interface
+		        Component component = (Component) e.getSource();
+		        OuvrirCompteForm frame = (OuvrirCompteForm) SwingUtilities.getWindowAncestor(component);
+		        
+//		        // alimentation du tableau des erreurs
+//		        Hashtable<String, List<String>> errors = compteService.checkFields(frame);
+//		        
+//		        // si il y a plusieurs champs non saisis
+//		        if((errors.get("emptyFields").size() > 0)) {
+//		        	
+//	                String error = errors.get("emptyFields").stream().collect(Collectors.joining(", "));
+//	                
+//	                JOptionPane.showMessageDialog(getContentPane(), 
+//	                      "Veuillez saisir le ou les champs suivants : " + error,
+//	                      "Erreur",
+//	                      JOptionPane.ERROR_MESSAGE);
+//	            // sinon si les champs ne sont pas numériques    
+//                }else if(errors.get("emptyFields").size() == 0 && errors.get("badNumericFields").size() > 0) {
+//                	
+//	                String error = errors.get("badNumericFields").stream().collect(Collectors.joining(", "));
+//	                
+//	                JOptionPane.showMessageDialog(getContentPane(), 
+//	                      "Le ou les champs suivants ne sont pas au format numérique : " + error,
+//	                      "Erreur",
+//	                      JOptionPane.ERROR_MESSAGE);
+//	            // sinon le champ solde initial est-il inférieur au champ soolde minimum
+//                }else if(rdbtnCompteCourant.isSelected() && Double.parseDouble(textFieldMontant.getText()) < Double.parseDouble(textFieldSoldeMinimum.getText())) {
+//                	
+//	                JOptionPane.showMessageDialog(getContentPane(), 
+//		                      "Le solde initial ne peut pas être inférieur au solde minimum autorisé !",
+//		                      "Avertissement",
+//		                      JOptionPane.WARNING_MESSAGE);
+//	            //sinon si le champ solde initial est-il supérieur au champ plafond    
+//                }else if(rdbtnCompteEpargne.isSelected() && Double.parseDouble(textFieldMontant.getText()) > Double.parseDouble(textFieldPlafond.getText())) {
+//                	
+//	                JOptionPane.showMessageDialog(getContentPane(), 
+//		                      "Le solde initial ne peut pas être supérieur au plafond autorisé !",
+//		                      "Avertissement",
+//		                      JOptionPane.WARNING_MESSAGE);
+//                }else if(rdbtnCompteEpargne.isSelected() && (Double.parseDouble(textFieldMontant.getText()) > 100000 || Double.parseDouble(textFieldPlafond.getText()) > 100000)) {
+//                	JOptionPane.showMessageDialog(getContentPane(), 
+//		                      "La valeur maximal des champs solde initial et plafond ne peut pas excéder 100000 !",
+//		                      "Avertissement",
+//		                      JOptionPane.WARNING_MESSAGE);
+//                }else if(rdbtnCompteEpargne.isSelected() && (Double.parseDouble(textFieldTauxInteret.getText()) > 100)) {
+//                	JOptionPane.showMessageDialog(getContentPane(), 
+//		                      "Le taux d'intérêt ne peut pas excéder 100% !",
+//		                      "Avertissement",
+//		                      JOptionPane.WARNING_MESSAGE);
+//                }
+//                else {
+//                	//vérifier que le champ taux d'intérêt est pas supérieur à 100
+//                	if(compteService.createBankAccount(frame)){
+//                    	JOptionPane.showMessageDialog(getContentPane(), 
+//  		                      "Le compte a bien été créé.",
+//  		                      "Information",
+//  		                      JOptionPane.INFORMATION_MESSAGE);
+//                    	
+//                    	compteService.fieldReinitialization(frame);
+//                    	comboBoxCompteSource.setSelectedIndex(0);
+//                    	compteService.generateNumCompte(textFieldNumCompte);
+//                	}else {
+//                    	JOptionPane.showMessageDialog(getContentPane(), 
+//    		                      "Une erreur s'est produite, le compte n'a pas été enregistré !",
+//    		                      "Erreur",
+//    		                      JOptionPane.ERROR_MESSAGE);
+//                	}
+//                }		        
+			}
+		});
+		
+		btnCreer.setBounds(497, 541, 133, 33);
+		subPanelForm.add(btnCreer);
+		
+		JButton btnReinitialiser = new JButton("Réinitialiser");
+		btnReinitialiser.setForeground(new Color(255, 255, 255));
+		btnReinitialiser.setBackground(new Color(30, 125, 125));
+		btnReinitialiser.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+		        Component component = (Component) e.getSource();
+		        TransfererForm frame = (TransfererForm) SwingUtilities.getWindowAncestor(component);
+		        
+//		        compteService.fieldReinitialization(frame);
+			}
+		});
+		
+		btnReinitialiser.setBounds(263, 541, 149, 33);
+		subPanelForm.add(btnReinitialiser);
+		
+		JButton btnRetour = new JButton("Retour");
+		btnRetour.setForeground(new Color(255, 255, 255));
+		btnRetour.setBackground(new Color(30, 125, 125));
+		btnRetour.setBounds(48, 541, 133, 33);
+		subPanelForm.add(btnRetour);
+		
+		comboBoxCompteSource = new JComboBox<String>();
+		comboBoxCompteSource.setForeground(Color.WHITE);
+		comboBoxCompteSource.setBackground(SystemColor.desktop);
+		comboBoxCompteSource.setBounds(150, 197, 370, 33);
+		comboBoxCompteSource.addItem("");
+		listCompteSource.put(-1, "");
+		
+		subPanelForm.add(comboBoxCompteSource);
+		
+		JLabel lblCompteSource = new JLabel("Sélectionnez le compte source :");
+		lblCompteSource.setBounds(150, 154, 230, 24);
+		subPanelForm.add(lblCompteSource);
+		
+		JSeparator separatorCompteSource = new JSeparator();
+		separatorCompteSource.setForeground(new Color(0, 102, 102));
+		separatorCompteSource.setBackground(SystemColor.desktop);
+		separatorCompteSource.setBounds(150, 177, 60, 15);
+		subPanelForm.add(separatorCompteSource);
+		
+		JLabel lblCompteDestinataire = new JLabel("Sélectionnez le compte destinataire :");
+		lblCompteDestinataire.setBounds(150, 283, 276, 24);
+		subPanelForm.add(lblCompteDestinataire);
+		
+		JSeparator separatorCompteDestinataire = new JSeparator();
+		separatorCompteDestinataire.setForeground(new Color(0, 102, 102));
+		separatorCompteDestinataire.setBackground(SystemColor.desktop);
+		separatorCompteDestinataire.setBounds(151, 307, 60, 15);
+		subPanelForm.add(separatorCompteDestinataire);
+		
+		comboBoxCompteDestinataire = new JComboBox<String>();
+		comboBoxCompteDestinataire.setForeground(Color.WHITE);
+		comboBoxCompteDestinataire.setBackground(SystemColor.desktop);
+		comboBoxCompteDestinataire.setBounds(150, 319, 370, 33);
+		comboBoxCompteDestinataire.addItem("");
+		listCompteDestinataire.put(-1, "");
+		
+		subPanelForm.add(comboBoxCompteDestinataire);
+		
+		pack();
+	}
+	
+	public int getListClientKey(String value) {
+		Integer id = -1;
+//	        if(entry.getValue().equals(value)){
+//	    for(Map.Entry<Integer, String> entry : this.listClients.entrySet()){
+//	            id = entry.getKey();
+//	        }
+//	    }
+	    
+	    return id;
+	}
+}
--- src/main/Program.java
+++ src/main/Program.java
@@ -1,9 +1,13 @@
 package main;
 
+import interfaces.TransfererForm;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		TransfererForm transfererForm = new TransfererForm();
+		transfererForm.setVisible(true);
 	}
 
 }
